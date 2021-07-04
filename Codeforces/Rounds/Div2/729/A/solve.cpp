27b3623cfcf6b728311725590bd8cfbcfa4b5112
ishank-katiyar <ishankkatiyar162@gmail.com>
2021-07-04 10:44:29 +0000

    codeforces_round_729

---
+++ Codeforces/Rounds/Div2/729/A/solve.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace std {
+
+template<class Fun>
+class y_combinator_result {
+	Fun fun_;
+public:
+	template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+	template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+} // namespace std
+
+int main() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int odd = 0;
+		for (int i = 0; i < 2 * n; i++) {
+			int x;
+			cin >> x;
+			odd += (x % 2);
+		}
+		cout << (odd == n ? "YES" : "NO") << '\n';
+	}
+	return 0;
+}
